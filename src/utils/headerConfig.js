7a90c31842c9c8192c346ab727f090a2bcd5bdf4
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-01 19:16:10 +0300

    feat: add key-value pairs

--- src/utils/headerConfig.js
+++ src/utils/headerConfig.js
@@ -1,9 +1,11 @@
 const headerConfig = {
   logoLink: 'https://marshakbooks.ru/',
   targetIsBlank: true,
+  titleText: "Музыкальный проект Турбина",
   marshakAltLogoText: 'Логотип книжного магазина Маршак',
   turbinaAltLogoText: 'Логотип музыкального проекта Турбина',
   menuButtonText: 'Стриминги',
+  menuButtonLabel: 'Открыть список стриминговых сервисов',
   menuLabel: 'Меню с ссылками на стриминговые сервисы',
   strimingsServices: [
     {
