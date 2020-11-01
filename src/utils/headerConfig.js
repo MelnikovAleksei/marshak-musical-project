455c6d5094f59bd786efb5bde86b547a4caf961d
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-01 19:02:57 +0300

    feat: create config for Header component

---
+++ src/utils/headerConfig.js
@@ -0,0 +1,36 @@
+const headerConfig = {
+  logoLink: 'https://marshakbooks.ru/',
+  targetIsBlank: true,
+  marshakAltLogoText: 'Логотип книжного магазина Маршак',
+  turbinaAltLogoText: 'Логотип музыкального проекта Турбина',
+  menuButtonText: 'Стриминги',
+  menuLabel: 'Меню с ссылками на стриминговые сервисы',
+  strimingsServices: [
+    {
+      title: 'Яндекс.Музыка',
+      url: 'https://music.yandex.ru/',
+      targetIsBlank: true,
+      lang: 'ru'
+    },
+    {
+      title: 'Spotify',
+      url: 'https://www.spotify.com/',
+      targetIsBlank: true,
+      lang: 'en'
+    },
+    {
+      title: 'Apple Music',
+      url: 'https://www.apple.com/ru/apple-music/',
+      targetIsBlank: true,
+      lang: 'en'
+    },
+    {
+      title: 'VK Music',
+      url: 'https://vk.com/',
+      targetIsBlank: true,
+      lang: 'en'
+    }
+  ]
+}
+
+export default headerConfig;
