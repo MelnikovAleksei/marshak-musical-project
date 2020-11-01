afd567cfbed44b5011ab5fc03962b7c22b73fce0
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-01 19:03:32 +0300

    feat: create a Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
-
+import marshakLogo from '../images/logo/marshak-logo.png';
+import turbinaLogo from '../images/logo/turbina-logo.png';
+import Header from './Header';
+import headerConfig from '../utils/headerConfig';
 import '../index.css';
 
 function App() {
+
+  const strimingsServicesList = headerConfig.strimingsServices;
+
   return (
     <div className="root">
-      <h1>Hello world!</h1>
+      <Header
+        config={headerConfig}
+        marshakLogo={marshakLogo}
+        turbinaLogo={turbinaLogo}
+      >
+        {strimingsServicesList.map((striming, index) =>
+          <li
+            className="header__menu-item"
+            key={index}
+          >
+            <a
+              className="header__menu-link"
+              href={striming.url}
+              target={striming.targetIsBlank && '_blank'}
+              rel="noopener"
+              lang={striming.lang}
+            >
+              {striming.title}
+            </a>
+          </li>
+        )}
+      </Header>
     </div>
   );
 }
