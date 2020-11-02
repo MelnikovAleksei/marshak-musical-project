159cc48fccf4be7bcd1796d3dea05136c8386d07
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-02 12:35:14 +0300

    feat: create markup for Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import Article from './Article';
+import articlesConfig from '../utils/articlesConfig';
 
 function Main() {
+
+  const articles = articlesConfig.articles;
+
   return (
     <main className="main">
-      <div>Player component</div>
-      <div>About component</div>
-      <div>How it works component</div>
-      <div>Theses component</div>
-      <div>Form component</div>
+      <div className="main__player-container">
+        <div>Player component</div>
+      </div>
+      <div className="main__about-container">
+        <section className="main__section articles">
+          {articles.map((article) =>
+            <Article
+              title={article.title}
+              key={article.id}
+            >
+              {article.paragraphs.map((paragraph) =>
+                <p
+                  className="article__paragraph"
+                  key={paragraph.id}
+                >
+                  {paragraph.text}
+                </p>
+              )}
+            </Article>
+          )}
+        </section>
+      </div>
+      <div className="main__form-container">
+        <section className="main__section forms">
+          <div>Form component</div>
+        </section>
+      </div>
     </main>
   )
 }
