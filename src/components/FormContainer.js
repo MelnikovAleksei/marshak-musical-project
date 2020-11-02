5882b55cb086330c4dad32f3ecae64d2ba8cc03d
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-02 20:48:08 +0300

    feat: refactor FormComponent and FormContainer

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import FormComponent from './FormComponent';
-import Input from './Input';
-import Textarea from './Textarea';
-import formConfig from '../utils/formConfig';
-
 
 class FormContainer extends React.Component {
   constructor() {
@@ -17,78 +13,43 @@ class FormContainer extends React.Component {
         offer: false
       }
     };
+    this.initialState = this.state;
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(evt) {
     const {name, value, type, checked} = evt.target;
-    type === 'checkbox' ?
-    this.setState({
-      agreements: {
-        [name]: checked
-      }
-    })
-    :
-    this.setState({
-      [name]: value
-    })
+    if(type === 'checkbox') {
+      this.setState({
+        agreements: {
+          [name]: checked
+        }
+      })
+    } else {
+        this.setState({
+          [name]: value
+        })
+    }
   }
 
   handleSubmit(evt) {
     evt.preventDefault();
-    alert(`${this.state.fullName}, ${this.state.email}, ${this.state.tel}, ${this.state.poems}, ${this.state.agreements.offer.toString()}`)
-    evt.target.reset();
+    alert(`${this.state.fullName}, ${this.state.tel}, ${this.state.poems}, ${this.state.email}, ${this.state.agreements.offer}`)
+    this.setState(this.initialState)
   }
 
   render() {
-      const fieldSets = formConfig.fieldsets.map((fieldset) =>
-        <fieldset aria-label={fieldset.label} key={fieldset.id}>
-          {fieldset.inputFields.map((input) => {
-            if (input.element === 'textarea') {
-              return (
-                <Textarea
-                  key={input.id}
-                  config={input}
-                  handleChange={this.handleChange}
-                  {...this.state}
-                />
-              )
-            } else {
-              return (
-                <Input
-                  key={input.id}
-                  config={input}
-                  handleChange={this.handleChange}
-                  {...this.state}
-                />
-              )
-            }
-          })}
-        </fieldset>
-      )
-
-      const buttons = formConfig.buttons.map((button) =>
-        <button
-          key={button.id}
-          aria-label={button.label}
-          type={button.type}
-        >
-          {button.title}
-        </button>
-      )
-
-
     return(
-      <FormComponent
-        key={formConfig.id}
-        handleSubmit={this.handleSubmit}
-        title={formConfig.title}
-        name={formConfig.name}
-      >
-        {fieldSets}
-        {buttons}
-      </FormComponent>
+      <>
+        <h2 className="title">Форма</h2>
+        <FormComponent
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+          {...this.state}
+        >
+        </FormComponent>
+      </>
     )
   }
 }
