3de200dd10b0946c71e7fc3baf70d1c31a14c0a5
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-02 10:57:41 +0300

    feat: create a template for the main block

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import marshakLogo from '../images/logo/marshak-logo.png';
 import turbinaLogo from '../images/logo/turbina-logo.png';
 import Header from './Header';
+import Main from './Main';
 import headerConfig from '../utils/headerConfig';
 import '../index.css';
 
@@ -33,6 +34,7 @@ function App() {
           </li>
         )}
       </Header>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main className="main">
+      <div>Player component</div>
+      <div>About component</div>
+      <div>How it works component</div>
+      <div>Theses component</div>
+      <div>Form component</div>
+    </main>
+  )
+}
+
+export default Main;
