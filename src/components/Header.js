cd4d80e033dd3a3022c052f0db99dc374f42cc5c
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-04 15:35:10 +0300

    feat: implement menu operation in App and Header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,18 +3,49 @@ import React from 'react';
 function Header(props) {
   return (
     <header className="header">
-      <a
-        className="header__logo-link"
-        href={props.config.logoLink}
-        target={props.config.targetIsBlank && '_blank'}
-        rel="noopener"
-      >
-        <img
-          className="header__logo-marshak"
-          alt={props.config.marshakAltLogoText}
-          src={props.marshakLogo}
-        />
-      </a>
+      <div className="header__menu-container">
+        <div className="header__logo-container">
+          <a
+            className="header__logo-link"
+            href={props.config.logoLink}
+            target={props.config.targetIsBlank && '_blank'}
+            rel="noopener"
+          >
+            <img
+              className="header__logo-marshak"
+              alt={props.config.marshakAltLogoText}
+              src={props.marshakLogo}
+            />
+          </a>
+        </div>
+        <div className="header__nav-container">
+          <button
+            className={props.menuIsOpen ?
+              "header__menu-button-active"
+              :
+              "header__menu-button"
+            }
+            aria-label={props.config.menuButtonLabel}
+            onClick={props.onMenuButtonClick}
+          >
+            {!props.menuIsOpen && props.config.menuButtonText}
+          </button>
+          <nav
+            className="header__menu"
+            aria-label={props.config.menuLabel}
+          >
+            <ul
+              className={props.menuIsOpen ?
+                "header__menu-list header__menu-list_opened"
+                :
+                "header__menu-list"
+                }
+            >
+              {props.children}
+            </ul>
+          </nav>
+        </div>
+      </div>
       <h1
         className="header__title"
         aria-label={props.config.titleText}
@@ -25,22 +56,6 @@ function Header(props) {
           src={props.turbinaLogo}
         />
       </h1>
-      <div className="header__menu-section">
-        <button
-          className="header__menu-button"
-          aria-label={props.config.menuButtonLabel}
-        >
-          {props.config.menuButtonText}
-        </button>
-        <nav
-          className="header__menu"
-          aria-label={props.config.menuLabel}
-        >
-          <ul className="header__menu-list">
-            {props.children}
-          </ul>
-        </nav>
-      </div>
     </header>
   )
 }
