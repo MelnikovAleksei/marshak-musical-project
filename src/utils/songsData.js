6c3b2e90d560be5156a1aeeb1b6392919e5b011b
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-06 18:55:22 +0300

    feat: create test data for audio player

---
+++ src/utils/songsData.js
@@ -0,0 +1,28 @@
+const songsData = [
+  {
+    id: 1,
+    lang: 'en',
+    authors: 'Frank Sinatra',
+    name: "That's life first",
+    text: "That's life, that's what all the people say\r\nYou're riding high in April, shot down in May \r \nBut I know I'm gonna change that tune \r\nWhen I'm back on top, back on top in June \r\nI said, that's life, and as funny as it may seem \r\nSome people get their kicks \r\nStompin' on a dream \r\nBut I don't let it, let it get me down \r\nCause this fine old world it keeps spinning around",
+    src: "../songs/frank-sinatra_that-s-life.mp3"
+  },
+  {
+    id: 2,
+    lang: 'en',
+    authors: 'Frank Sinatra',
+    name: "That's life second",
+    text: "That's life, that's what all the people say\r\nYou're riding high in April, shot down in May \r \nBut I know I'm gonna change that tune \r\nWhen I'm back on top, back on top in June \r\nI said, that's life, and as funny as it may seem \r\nSome people get their kicks \r\nStompin' on a dream \r\nBut I don't let it, let it get me down \r\nCause this fine old world it keeps spinning around",
+    src: "../songs/frank-sinatra_that-s-life.mp3"
+  },
+  {
+    id: 1,
+    lang: 'en',
+    authors: 'Frank Sinatra',
+    name: "That's life third",
+    text: "That's life, that's what all the people say\r\nYou're riding high in April, shot down in May \r \nBut I know I'm gonna change that tune \r\nWhen I'm back on top, back on top in June \r\nI said, that's life, and as funny as it may seem \r\nSome people get their kicks \r\nStompin' on a dream \r\nBut I don't let it, let it get me down \r\nCause this fine old world it keeps spinning around",
+    src: "../songs/frank-sinatra_that-s-life.mp3"
+  },
+]
+
+export default songsData;
