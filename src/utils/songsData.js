5b962a6dd3c96b65343afd386de2987b3e68a710
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-13 07:56:49 +0300

    feat: change the names of files, variables; add a video file; change the structure of songsData

--- src/utils/songsData.js
+++ src/utils/songsData.js
@@ -1,6 +1,8 @@
-import frank_sinatra_that_s_life from '../songs/frank-sinatra_that-s-life.mp3';
-import frank_sinatra_moon_river from '../songs/Frank-Sinatra-Moon-River.mp3';
-import frank_sinatra_my_way from '../songs/Frank-Sinatra-My-Way.mp3';
+import fs_t_l_audio from '../songs/fs_t-l.mp3';
+import fs_m_r_audio from '../songs/fs_m-r.mp3';
+import fs_m_w_audio from '../songs/fs_m-w.mp3';
+
+import fs_w_o_w_video from '../videos/fs_w-o-w.mp4';
 
 const songsData = [
   {
@@ -9,7 +11,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "That's life",
     text: "That's life, that's what all the people say\nYou're riding high in April, shot down in May\nBut I know I'm gonna change that tune\nWhen I'm back on top, back on top in June\nI said, that's life, and as funny as it may seem\nSome people get their kicks\nStompin' on a dream\nBut I don't let it, let it get me down\nCause this fine old world it keeps spinning around",
-    src: frank_sinatra_that_s_life
+    audioSrc: fs_t_l_audio,
+    video: true,
+    videoSrc: fs_w_o_w_video
   },
   {
     id: 2,
@@ -17,7 +21,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "Moon River",
     text: "Moon river, wider than a mile\nI'm crossing you in style some day\nOh, dream maker, you heart breaker\nWherever you're goin', I'm goin' your way",
-    src: frank_sinatra_moon_river
+    audioSrc: fs_m_r_audio,
+    video: true,
+    videoSrc: fs_w_o_w_video
   },
   {
     id: 3,
@@ -25,7 +31,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "My Way",
     text: "And now the end is near\nAnd so I face the final curtain\nMy friend I'll say it clear\nI'll state my case of which I'm certain",
-    src: frank_sinatra_my_way
+    audioSrc: fs_m_w_audio,
+    video: true,
+    videoSrc: fs_w_o_w_video
   },,
   {
     id: 4,
@@ -33,7 +41,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "My Way",
     text: "And now the end is near\nAnd so I face the final curtain\nMy friend I'll say it clear\nI'll state my case of which I'm certain",
-    src: frank_sinatra_my_way
+    audioSrc: fs_m_w_audio,
+    video: true,
+    videoSrc: fs_w_o_w_video
   },
   {
     id: 5,
@@ -41,7 +51,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "My Way",
     text: "And now the end is near\nAnd so I face the final curtain\nMy friend I'll say it clear\nI'll state my case of which I'm certain",
-    src: frank_sinatra_my_way
+    audioSrc: fs_m_w_audio,
+    video: true,
+    videoSrc: fs_w_o_w_video
   },
   {
     id: 6,
@@ -49,7 +61,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "My Way",
     text: "And now the end is near\nAnd so I face the final curtain\nMy friend I'll say it clear\nI'll state my case of which I'm certain",
-    src: frank_sinatra_my_way
+    audioSrc: fs_m_w_audio,
+    video: false,
+    videoSrc: ''
   },
   {
     id: 7,
@@ -57,7 +71,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "My Way",
     text: "And now the end is near\nAnd so I face the final curtain\nMy friend I'll say it clear\nI'll state my case of which I'm certain",
-    src: frank_sinatra_my_way
+    audioSrc: fs_m_w_audio,
+    video: false,
+    videoSrc: ''
   },
   {
     id: 8,
@@ -65,7 +81,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "My Way",
     text: "And now the end is near\nAnd so I face the final curtain\nMy friend I'll say it clear\nI'll state my case of which I'm certain",
-    src: frank_sinatra_my_way
+    audioSrc: fs_m_w_audio,
+    video: false,
+    videoSrc: ''
   },
   {
     id: 9,
@@ -73,7 +91,9 @@ const songsData = [
     authors: 'Frank Sinatra',
     name: "My Way",
     text: "And now the end is near\nAnd so I face the final curtain\nMy friend I'll say it clear\nI'll state my case of which I'm certain",
-    src: frank_sinatra_my_way
+    audioSrc: fs_m_w_audio,
+    video: false,
+    videoSrc: ''
   }
 ]
 
